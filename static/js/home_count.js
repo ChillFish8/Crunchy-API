1e02fc654c38e88474287976b0bb4cc0e01521e8
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-06-11 22:24:21 +0100

    front page

--- static/js/home_count.js
+++ static/js/home_count.js
@@ -1,4 +1,4 @@
-import { CountUp } from './countUp.js/dist/countUp.min';
+import { CountUp } from './countUp/dist/countUp.min';
 
 window.onload = function() {
   var countUp = new CountUp('counter-main-users', 2000);
