9315ef1fdb0ce1833636de9fadcb8027306d1b93
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-06-14 18:19:31 +0100

    added guild listing with oath

--- main_site/dashboard.py
+++ main_site/dashboard.py
@@ -2,6 +2,7 @@ import jinja2_sanic
 from sanic.request import Request
 
 from globals import Globals
+from sanic_oauth.blueprint import fetch_user_guilds
 from sanic_oauth.blueprint import login_required
 from sanic_oauth.core import UserInfo
 
@@ -12,26 +13,25 @@ app = Globals.app
 @login_required(provider="discord")
 async def dashboard_home(request: Request, user_info: UserInfo):
     user_info = user_info.__dict__
+
     user_icon = f'https://cdn.discordapp.com/avatars/{user_info["id"]}/{user_info["avatar"]}.png'
     username = user_info['username']
+
+    user, guilds = await fetch_user_guilds(request, provider="discord",
+                                           oauth_endpoint_path="https://discord.com/api/users/@me/guilds")
+    guild_data = []
+    for guild in guilds:
+        guild_data.append(
+            {
+                'name': guild['name'],
+                'url': f'https://cdn.discordapp.com/icons/{guild["id"]}/{guild["icon"]}.webp?size=256'
+            }
+        )
     context = {
         'logged_in': True,
         'user': username,
         'icon': user_icon,
-        'recent_guilds': [
-            {
-                'url': 'https://cdn.discordapp.com/icons/675647130647658527/61515ba4de15b723324b5d7cb8754ed1.webp?size=256',
-                'name': "Crunchy's Room"
-            },
-            {
-                'url': 'https://cdn.discordapp.com/icons/267624335836053506/a_0409ea649c9eb2aaaf3a5531a80a2528.webp?size=256',
-                'name': "Python"
-            },
-            {
-                'url': 'https://cdn.discordapp.com/icons/267624335836053506/a_0409ea649c9eb2aaaf3a5531a80a2528.webp?size=256',
-                'name': "Python"
-            },
-        ],
+        'recent_guilds': guild_data,
     }
     resp = jinja2_sanic.render_template("templates.dashboard_home", request, context)
     resp.headers['Access-Control-Allow-Origin'] = '*'
